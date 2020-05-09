58897beb1bcaf9cc4dbbf91499ec1a010178fd1a
Tom Titcombe <t.j.titcombe@gmail.com>
2020-05-09 14:03:53 +0100

    Add laplacian noise to classifier

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 """
 Neural networks
 """
+import numpy as np
 import torch
 
 
@@ -10,21 +11,30 @@ class SplitNN(torch.nn.Module):
 
         self.noise_scale = noise_scale
 
-        self.part1 = torch.nn.Sequential(torch.nn.Linear(784, 500), torch.nn.ReLU(),)
-
-        # TODO add noise
+        self.part1 = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 32, 3, 1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(32, 64, 3, 1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Flatten(),
+        )
 
         self.part2 = torch.nn.Sequential(
-            torch.nn.Linear(500, 128),
+            torch.nn.Linear(9216, 128),
             torch.nn.ReLU(),
             torch.nn.Linear(128, 10),
             torch.nn.Softmax(dim=1),
         )
 
     def forward(self, x):
-        x = x.view(-1, 784)
-        return self.part2(self.part1(x))
+        return self.part2(self.encode(x))
 
     def encode(self, x):
-        x = x.view(-1, 784)
-        return self.part1(x)
+        out = self.part1(x)
+        out += torch.tensor(
+            np.random.laplace(loc=0.0, scale=self.noise_scale, size=tuple(out.size())),
+            requires_grad=True,
+        ).float()
+
+        return out
