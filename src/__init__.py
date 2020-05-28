2642fbedd3e30f09cca5a6e819bc239174efb091
Tom Titcombe <t.j.titcombe@gmail.com>
2020-05-28 16:23:44 +0100

    Give model tanh act function to bound data for DP
    Create relu only version of SplitNN for comparison

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .attacker import AttackDataset, AttackModel, ConvAttackModel
-from .models import SplitNN
+from .models import SplitNN, ReLUSplitNN
 from .nopeek_loss import NoPeekLoss
 from .visualisation import plot_images
--- src/models.py
+++ src/models.py
@@ -18,7 +18,7 @@ class SplitNN(torch.nn.Module):
             torch.nn.MaxPool2d(2),
             torch.nn.Flatten(),
             torch.nn.Linear(9216, 500),
-            torch.nn.ReLU(),
+            torch.nn.Tanh(),
         )
 
         self.part2 = torch.nn.Sequential(
@@ -38,7 +38,10 @@ class SplitNN(torch.nn.Module):
 
     def forward(self, x):
         intermediate = self.part1(x)
-        out = self.part2(intermediate + self._noise.sample(intermediate.size()).to(intermediate.device))
+        out = self.part2(
+            intermediate
+            + self._noise.sample(intermediate.size()).to(intermediate.device)
+        )
         return out, intermediate
 
     def encode(self, x):
@@ -49,3 +52,28 @@ class SplitNN(torch.nn.Module):
 
     def decode(self, x):
         return self.part2(x)
+
+
+class ReLUSplitNN(SplitNN):
+    def __init__(self, noise_scale: float = 0.0) -> None:
+        super().__init__()
+
+        self._noise = torch.distributions.Laplace(0.0, noise_scale)
+
+        self.part1 = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 32, 3, 1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(32, 64, 3, 1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Flatten(),
+            torch.nn.Linear(9216, 500),
+            torch.nn.ReLU(),
+        )
+
+        self.part2 = torch.nn.Sequential(
+            torch.nn.Linear(500, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 10),
+            torch.nn.Softmax(dim=1),
+        )
