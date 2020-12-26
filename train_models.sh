c03b4963ec3a8a009dec977d7fd696824a6ae166
Tom Titcombe <t.j.titcombe@gmail.com>
2020-12-26 20:43:11 +0000

    Add nopeek to training bash script
    Add arg to select training runs

--- train_models.sh
+++ train_models.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-for noise in 0.0 0.1 0.2 0.5 1.0
-do
-    echo "Training model with noise $noise"
-    python train/train_model.py --noise_scale $noise --nopeek_weight 0.0
-done
+if [ $1 = 'noise' ] || [ $1 = 'all' ]; then
+    for noise in 0.0 0.1 0.2 0.5 1.0
+    do
+        echo "Training model with noise $noise"
+        python train/train_model.py --noise_scale $noise --nopeek_weight 0.0
+    done
+fi
+
+# NoPeek is very computationally expensive!
+if [ $1 = 'nopeek' ] || [ $1 = 'all' ]; then
+    for nopeek in 0.1 0.5 1.0 5.0
+    do
+        echo "Training model with nopeek weight $nopeek"
+        python train/train_model.py --noise_scale 0.0 --nopeek_weight $nopeek --batch_size 32
+    done
+fi
