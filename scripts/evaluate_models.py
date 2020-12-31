05895d2bebca7162cbb686cb7f176c32bafaf2c7
Tom Titcombe <t.j.titcombe@gmail.com>
2020-12-31 13:50:21 +0000

    Fix: Correct acc & DCorr calc in evaluation script

--- scripts/evaluate_models.py
+++ scripts/evaluate_models.py
@@ -3,6 +3,7 @@ Evaluate MNIST classifiers in terms of accuracy and
 Distance Correlation (input and intermediate tensor)
 """
 import argparse
+import logging
 import os
 from pathlib import Path
 from typing import List, Tuple
@@ -21,20 +22,20 @@ def _evaluate_model_accuracy(model):
 
     for x, y in model.train_dataloader():
         with torch.no_grad():
-            _, y_hat = model(x)
+            y_hat, _ = model(x)
 
         train_accuracy(y_hat, y)
 
     for x, y in model.val_dataloader():
         with torch.no_grad():
-            _, y_hat = model(x)
+            y_hat, _ = model(x)
 
         valid_accuracy(y_hat, y)
 
     total_train_accuracy = train_accuracy.compute()
     total_valid_accuracy = valid_accuracy.compute()
 
-    return total_train_accuracy, total_valid_accuracy
+    return total_train_accuracy.item() * 100, total_valid_accuracy.item() * 100
 
 
 def _evaluate_distance_correlation(model) -> Tuple[List, List]:
@@ -44,7 +45,7 @@ def _evaluate_distance_correlation(model) -> Tuple[List, List]:
 
     for x, _ in model.train_dataloader():
         with torch.no_grad():
-            intermediate, _ = model(x)
+            _, intermediate = model(x)
 
         dcorr_train.append(distance_correlation(x, intermediate))
 
@@ -52,15 +53,15 @@ def _evaluate_distance_correlation(model) -> Tuple[List, List]:
 
     for x, _ in model.val_dataloader():
         with torch.no_grad():
-            intermediate, _ = model(x)
+            _, intermediate = model(x)
 
         dcorr_valid.append(distance_correlation(x, intermediate))
 
     return (
-        np.mean(dcorr_train),
-        np.std(dcorr_train) / np.sqrt(len(dcorr_train)),
-        np.mean(dcorr_valid),
-        np.std(dcorr_valid) / np.sqrt(len(dcorr_valid)),
+        round(np.mean(dcorr_train), 3),
+        round(np.std(dcorr_train) / np.sqrt(len(dcorr_train)), 3),
+        round(np.mean(dcorr_valid), 3),
+        round(np.std(dcorr_valid) / np.sqrt(len(dcorr_valid)), 3),
     )
 
 
@@ -87,38 +88,51 @@ def _evaluate_models(models_path: Path, results_path: Path) -> None:
         ]
     )
 
-    for model_path in models_path.glob("*.ckpt"):
-        print(model_path.stem)
-        model = _load_model(model_path)
+    try:
+        for model_path in models_path.glob("*.ckpt"):
+            logging.info(f"Benchmarking {model_path.stem}")
 
-        train_acc, val_acc = _evaluate_model_accuracy(model)
-        (
-            train_dcorr_mean,
-            train_dcorr_se,
-            val_dcorr_mean,
-            val_dcorr_se,
-        ) = _evaluate_distance_correlation(model)
+            model = _load_model(model_path)
 
-        model_results = {
-            "Model": model_path.stem,
-            "MeanTrainAcc": train_acc,
-            "SETrainAcc": None,
-            "MeanValAcc": val_acc,
-            "SEValAcc": None,
-            "MeanTrainDCorr": train_dcorr_mean,
-            "SETrainDcorr": train_dcorr_se,
-            "MeanValDCorr": val_dcorr_mean,
-            "SEValDCorr": val_dcorr_se,
-        }
+            train_acc, val_acc = _evaluate_model_accuracy(model)
+            logging.info(
+                f"{model_path.stem} - Train acc: {train_acc:.3f}; Val acc: {val_acc:.3f}"
+            )
 
-        results = results.append(model_results, ignore_index=True)
+            (
+                train_dcorr_mean,
+                train_dcorr_se,
+                val_dcorr_mean,
+                val_dcorr_se,
+            ) = _evaluate_distance_correlation(model)
+            logging.info(
+                f"{model_path.stem} - Train DCorr: {train_dcorr_mean} +/- {train_dcorr_se}; Val DCorr: {val_dcorr_mean} +/- {val_dcorr_se}"
+            )
 
-        break
+            model_results = {
+                "Model": model_path.stem,
+                "MeanTrainAcc": train_acc,
+                "SETrainAcc": None,
+                "MeanValAcc": val_acc,
+                "SEValAcc": None,
+                "MeanTrainDCorr": train_dcorr_mean,
+                "SETrainDCorr": train_dcorr_se,
+                "MeanValDCorr": val_dcorr_mean,
+                "SEValDCorr": val_dcorr_se,
+            }
+
+            results = results.append(model_results, ignore_index=True)
+    except KeyboardInterrupt:
+        pass
 
     results.to_csv(results_path / "model_performances.csv", index=False)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s %(message)s", level=logging.INFO, datefmt="%I:%M:%S"
+    )
+
     project_root = Path(__file__).parents[1]
     models_path = project_root / "models" / "classifiers"
     results_path = project_root / "results"
