cc24cf77b1c3052686bf013eead841cf69a3c59b
Tom Titcombe <t.j.titcombe@gmail.com>
2021-01-03 15:40:57 +0000

    Improvement: add blank lines to echo in main.sh
    Easier to see which experiment is running

--- main.sh
+++ main.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $1 = 'plain' ] || [ $1 = 'all' ]; then
-    echo "Training model without noise or NoPeek"
+    echo -e "\nTraining model without noise or NoPeek\n"
     python train/train_model.py --noise_scale 0.0 --nopeek_weight 0.0
 fi
 
 if [ $1 = 'noise' ] || [ $1 = 'all' ]; then
     for noise in 0.1 0.2 0.5 1.0 2.0 5.0
     do
-        echo "Training model with noise $noise"
+        echo -e "\nTraining model with noise $noise\n"
         python scripts/train_model.py --noise_scale $noise --nopeek_weight 0.0
     done
 fi
@@ -17,23 +17,27 @@ fi
 if [ $1 = 'nopeek' ] || [ $1 = 'all' ]; then
     for nopeek in 0.05 0.1 0.25 0.5 1.0
     do
-        echo "Training model with nopeek weight $nopeek"
+        echo -e "\nTraining model with nopeek weight $nopeek\n"
         python scripts/train_model.py --noise_scale 0.0 --nopeek_weight $nopeek --batch_size 32
     done
 fi
 
+# NoPeek is very computationally expensive!
 if [ $1 = 'combo' ] || [ $1 = 'all' ]; then
     for noise in 0.1 0.2 0.5 1.0
     do
         for nopeek in 0.1 0.25 0.5
         do
-            echo "Training model with noise $noise and nopeek weight $nopeek"
+            echo -e "\nTraining model with noise $noise and nopeek weight $nopeek\n"
             python scripts/train_model.py --noise_scale $noise --nopeek_weight $nopeek --batch_size 32
         done
     done
 fi
 
 if [ $1 = 'performance' ] || [ $1 = 'all' ]; then
-    echo "Calculating model performances"
+    echo -e "\nEvaluating classifier performances\n"
     python scripts/evaluate_models.py
+
+    echo -e "\nEvaluating attack performances\n"
+    python scripts/evaluate_attackers.py
 fi
